a65abc1300e483d9a61d98a255d23e3025141771
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2022-12-01 00:00:27 +0700

    Add UserService

--- petshop/src/main/java/tdtu/petshop/services/UserService.java
+++ petshop/src/main/java/tdtu/petshop/services/UserService.java
@@ -1,5 +1,29 @@
 package tdtu.petshop.services;
 
-public class UserService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import tdtu.petshop.models.User;
+import tdtu.petshop.repositories.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	public User findByPhone(String phone) {
+		return userRepository.findByPhone(phone);
+	}
+	
+	public void addUser(User user) {
+		userRepository.save(user);
+	}
+		
 }
