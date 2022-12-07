f3c965e55af84894e6009ee8d2eef494cd48d70a
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2022-12-07 20:59:45 +0700

    Add edit staff basic info

--- petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
+++ petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
@@ -2,14 +2,21 @@ package tdtu.petshop.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import tdtu.petshop.models.User;
 import tdtu.petshop.services.RoleService;
@@ -40,9 +47,28 @@ public class AdminController {
 		return "redirect:/admin/staff";
 	}
 	
+	@GetMapping(path = "/staff/edit/{username}", produces = MimeTypeUtils.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> getStaffEdit(@PathVariable("username") String username) {
+		return new ResponseEntity<User>(userService.findByUsername(username), HttpStatus.OK);
+	}
+	
+	@PostMapping("/staff/edit")
+	public String postStaffEdit(@ModelAttribute("staff") User staff) {
+		User temp = userService.findByUsername(staff.getUsername());
+		temp.setPhone(staff.getPhone());
+		temp.setName(staff.getName());
+		userService.saveUser(temp);
+		return "redirect:/admin/staff";
+	}
+	
+	@GetMapping("/staff/edit/password/{username}")
+	public String getStaffEditPassword(@PathVariable("username") String username) {
+		return "password";
+	}
+	
 	@PostMapping("/staff/delete")
-	public String postDeleteStaff(@ModelAttribute("staff") User staff) {
-		userService.deleteUser(staff.getUsername());
+	public String postDeleteStaff(HttpServletRequest request) {
+		userService.deleteUser(request.getParameter("username"));
 		return "redirect:/admin/staff";
 	}
 	
