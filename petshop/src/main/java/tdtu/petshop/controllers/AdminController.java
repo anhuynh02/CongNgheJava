ce494017da4f58d80fb157e7342b4113505329f9
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2022-12-07 19:16:01 +0700

    Add function add Staff

--- petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
+++ petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
@@ -3,9 +3,12 @@ package tdtu.petshop.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tdtu.petshop.models.User;
@@ -26,6 +29,15 @@ public class AdminController {
 		List<User> staffs = userService.findAllByRole(roleService.findById(2));
 		model.addAttribute("staffs", staffs);  
 		return "staffManagement";
+	}
+	
+	@PostMapping("/staff/add")
+	public String postAddStaff(@ModelAttribute("staff") User staff) {
+		staff.setPassword(new BCryptPasswordEncoder().encode(staff.getPassword()));
+		staff.setEnable(true);
+		staff.setRole(roleService.findById(2));
+		userService.saveUser(staff);
+		return "redirect:/admin/staff";
 	}
 	
 }
