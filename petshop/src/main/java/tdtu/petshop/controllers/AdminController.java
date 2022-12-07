22cb2a9fa541e19aacd3e5a1353f24b88390a36c
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2022-12-07 16:23:55 +0700

    Add temporary Staff Management template

---
+++ petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
@@ -0,0 +1,16 @@
+package tdtu.petshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@GetMapping("/staff")
+	public String getStaff() {
+		return "staffManagement";
+	}
+	
+}
