f9aaaec138ee8886e767fdcf2dbd78a965cf56c3
LiaCyrus <118715767+LiaCyrus@users.noreply.github.com>
2022-12-13 16:07:25 +0700

    Minor Change to service

--- petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
+++ petshop/src/main/java/tdtu/petshop/controllers/AdminController.java
@@ -37,7 +37,7 @@ public class AdminController {
     
     @GetMapping("")
 	public String getAdmin(Model model) {
-    	List<User> staffs = userService.findAllByRole(roleService.findById(2));
+    	List<User> staffs = userService.findAllByRole(2);
     	List<Product> products = productService.findAll();
     	model.addAttribute("staffs", staffs);
     	model.addAttribute("products",products);
@@ -46,7 +46,7 @@ public class AdminController {
 	
 	@GetMapping("/staff")
 	public String getStaff(Model model) {
-		List<User> staffs = userService.findAllByRole(roleService.findById(2));
+		List<User> staffs = userService.findAllByRole(2);
 		model.addAttribute("staffs", staffs);  
 		return "staffManagement";
 	}
--- petshop/src/main/java/tdtu/petshop/services/UserService.java
+++ petshop/src/main/java/tdtu/petshop/services/UserService.java
@@ -1,13 +1,11 @@
 package tdtu.petshop.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import tdtu.petshop.models.Role;
 import tdtu.petshop.models.User;
 import tdtu.petshop.repositories.UserRepository;
 
@@ -17,6 +15,8 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private RoleService roleService;
 	
 	public User findByUsername(String username) {
 		return userRepository.findByUsername(username);
@@ -26,8 +26,8 @@ public class UserService {
 		return userRepository.findByPhone(phone);
 	}
 	
-	public List<User> findAllByRole(Role role) {
-		return userRepository.findAllByRole(role);
+	public List<User> findAllByRole(int id) {
+		return userRepository.findAllByRole(roleService.findById(id));
 	}
 	
 	public void saveUser(User user) {
